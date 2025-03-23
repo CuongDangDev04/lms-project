76f66e4746bc664ab51a276da1d47f29290f1eef
huynhlong204 <huynhlongtc2@gmail.com>
2025-03-23 23:01:43 +0700

    Remove authorization middleware from course retrieval and creation routes

--- server/src/routes/admin/courseRoute.js
+++ server/src/routes/admin/courseRoute.js
@@ -5,8 +5,8 @@ const { verifyTokenAndRole } = require('../../middlewares/auth.middleware');
 const upload = require('../../middlewares/upload');
 
 router.get('/', courseController.getCourses);
-router.get("/:id", verifyTokenAndRole(3), courseController.getCourseByID);
-router.post("/create" , courseController.createCourse);
+router.get("/:id", courseController.getCourseByID);
+router.post("/create", courseController.createCourse);
 router.post("/createByExcel", upload.single("file"), courseController.createCourseByExcel);
 router.put("/:id", verifyTokenAndRole(3), courseController.updateCourse);
 router.delete('/:id', verifyTokenAndRole(3), courseController.deleteCourseById);
