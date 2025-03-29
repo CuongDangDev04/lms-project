feaadda8c4bb97a0837e65e9ac2c7063035528b2
huynhlongtc2 <huynhlongtc2@gmail.com>
2025-03-29 09:28:09 +0700

    feat: enhance getAllStudentsInClassroom to include user roles in response

--- server/src/controllers/users/memberController.js
+++ server/src/controllers/users/memberController.js
@@ -1,17 +1,25 @@
-const { Classroom, UserParticipation, User } = require("../../models/index");
+const { Classroom, UserParticipation, User, Role } = require("../../models/index");
 
 const getAllStudentsInClassroom = async (req, res) => {
-  const {classroomId}= req.params;
+  const { classroomId } = req.params;
   try {
     const classroom = await Classroom.findOne({
       where: { classroom_id: classroomId },
       include: [
         {
           model: User,
+          where: { role_id: 1 },
           through: UserParticipation,
+          include: [
+            {
+              model: Role,
+              attributes: ["role_name"],
+            },
+          ],
         },
       ],
     });
+
     if (!classroom) {
       return res.status(404).json({
         success: false,
@@ -19,13 +27,10 @@ const getAllStudentsInClassroom = async (req, res) => {
       });
     }
 
-    // Lấy danh sách user trực thuộc classroom
-    const students = classroom.Users;
-
     res.status(200).json({
       success: true,
       message: "Lấy tất cả sinh viên trực thuộc classroom thành công!",
-      data: students,
+      data: classroom.Users, // Trả về danh sách sinh viên
     });
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
